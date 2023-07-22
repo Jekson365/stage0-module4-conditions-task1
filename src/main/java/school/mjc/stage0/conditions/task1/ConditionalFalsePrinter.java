20fc256c4fc63d25529a19875de2036ba2d63282
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-07-22 10:44:56 +0400

    asd

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        if (3 < 2) {
+
+        }
+        else {
+            System.out.println("false");
+        }
     }
 }
